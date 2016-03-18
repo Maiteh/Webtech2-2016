59a34e055e7d5926c3a8343ce9fd8d780d468319
Maïteh Mertens <maiteh_m@hotmail.com>
2016-03-18 20:43:12 +0100

    Added gitignore copy of lab4, because PHP storm adds a bunch of files. Edited framework.js ordened the { the way i like to work :)

--- LAB3 - Advanced JS/js/framework.js
+++ LAB3 - Advanced JS/js/framework.js
@@ -1,6 +1,5 @@
 /* ---------------------------------------- FRAMEWORK ---------------------------------- */
-var WrapperElement = function(element)
-{
+var WrapperElement = function(element) {
     // a wrapper element allow us to extend html dom functionality
     // without changing the behaviour of built-in elements
 	
@@ -8,33 +7,26 @@ var WrapperElement = function(element)
     this.element = element;									
     
     // this allows us to see if a selection contains one or more elements
-    if(element.length > 1)
-    {
+    if(element.length > 1) {
         this.isArray = true;
     }
-    else
-    {
+    else {
         this.isArray = false;
     }
 }
 
-WrapperElement.prototype.toggleClass = function(className)
-{
+WrapperElement.prototype.toggleClass = function(className) {
     
 }
 
-WrapperElement.prototype.addClass = function(className)
-{
-	if(this.isArray)
-	{
+WrapperElement.prototype.addClass = function(className) {
+	if(this.isArray) {
         // multiple elements, we'll need to loop
-		for(var i = 0; i<this.element.length; i++)
-		{
+		for(var i = 0; i<this.element.length; i++) {
 			this.element[i].className += " " + className;
 		}
 	}
-	else
-	{
+	else {
         // just one element, so we can manipulate it without looping
 		this.element.className = className;
 	}
@@ -42,43 +34,35 @@ WrapperElement.prototype.addClass = function(className)
 	return this;
 }
 
-WrapperElement.prototype.prepend = function(item)
-{
+WrapperElement.prototype.prepend = function(item) {
 
 }
 
 WrapperElement.prototype.keyup = function(action){
-	if(this.isArray)
-	{
+	if(this.isArray) {
 		// multiple elements, we'll need to loop
-		for(var i = 0; i<this.element.length; i++)
-		{
+		for(var i = 0; i<this.element.length; i++) {
 			this.element[i].addEventListener('keyup', action);
 		}
 	}
-	else
-	{
+	else {
 		// just one element, let's go nuts
 		this.element.addEventListener('keyup', action);
 	}
 	return this;
 }
 
-WrapperElement.prototype.click = function(action)
-{
+WrapperElement.prototype.click = function(action) {
 
 }
 
-WrapperElement.prototype.val = function(value)
-{
+WrapperElement.prototype.val = function(value) {
 	
 }
 
-var $ = function(selector)
-{
+var $ = function(selector) {
 	// check if selector is an object already e.g. by passing 'this' on clicks
-	if(typeof(selector) == "object")
-	{
+	if(typeof(selector) == "object") {
 		return new WrapperElement(selector);
 	}
 
