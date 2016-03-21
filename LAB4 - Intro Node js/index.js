e982f54be0068804966e2374b9d2e5a6b0454fc1
Maïteh Mertens <maiteh_m@hotmail.com>
2016-03-21 23:05:54 +0100

    Edited lab4 inex.js, overviewing the part I had of the introduction with district01.

--- LAB4 - Intro Node js/index.js
+++ LAB4 - Intro Node js/index.js
@@ -1,24 +1,36 @@
+//importing modules we want to have
+var chalk = require('chalk');
 var express = require('express');
+var ejs = require('ejs');
+
+//creating the app
 var app = express();
-var bodyParser = require('body-parser');
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/nodeINTRO');
-
-// parse application/json , define this BEFORE adding routes
-app.use(bodyParser.json());
+/**
+ * Now we need to register ejs as our view engine
+ * express will use our ejs files from the view folder
+ * an example is the index url.
+ */
+app.set('view engine', 'ejs');
 
 /**
-* Verwijzen naar routers index.js voor Route naar index.
-* Index mag normaal weggelaten worden.
-*/
-app.use('/', require('./routers/index'));
-/**
-* Verwijzen naar routers messages.js voor Route naar messages.
-* Daarin komen alle links voor de posts en de gets en dergelijke. 
-*/
-app.use('/messages', require('./routers/messages'));
+ * This hosts a static folder, like css and images
+ * these public folders will be hosted on the root
+ * so that it's always available on "/"
+ */
+app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+/**
+ * Body Parser
+ * wil parse JSON that we post
+ * makes it available for requests of req.body
+ */
+ app.use(require('body-parser').json());
+
+//include a router
+app.use('/', require('./routers'));
+app.use('/users', require('./routers/users'));
+
+//Setting up the server on port 3000
+app.listen(3000, function(){
+    console.log(chalk.blue('The app is available on' + chalk.bold.yellow('http://localhost:3000') + '!'));
 });
