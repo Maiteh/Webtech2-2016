a29c96f7e20771c5045e84b7898cf7f70c44b6ac
Maïteh Mertens <maiteh_m@hotmail.com>
2016-03-21 23:23:56 +0100

    Added lab4 route.user.js

---
+++ LAB4 - Intro Node js/routers/user.js
@@ -0,0 +1,21 @@
+/**
+ * Every router file must require express
+ * otherwise it wil not recognize express
+ * express.router is the router that handles ALL routers
+ * NOW you can get and set on var router
+ * access the router object, so that we can add routes to it
+ * All functionality must be in the controller, routers are only for url's
+*/
+var controller = require('./../controllers/user');
+var express = require('express');
+var router = express.Router();
+
+router.get('/', controller.read);
+// Go to specific user
+router.get('/:id', controller.readOne);
+// this route is the same user/user, to make it clean
+router.post('/', controller.create);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.remove);
+
+module.exports = router;
