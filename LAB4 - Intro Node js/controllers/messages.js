bc1ba7cc52e920b05c9126a5698677dbb6f453a7
Maïteh Mertens <maiteh_m@hotmail.com>
2016-04-10 23:38:32 +0200

    Added lab4> controller messages, copied and modified users controller for messages

---
+++ LAB4 - Intro Node js/controllers/messages.js
@@ -0,0 +1,62 @@
+//A modern JS library.
+var _ = require('lodash');
+var Message = require('./../models/message');
+
+// get all messages
+function getAll(req, res, next) {
+
+    Message
+        .find({})
+        .exec(function onMessagesFound(err, messages) {
+            //Now return the a clean json to users instead of sending everything
+            //models/message.js for json
+            var transform = _.map(messages, function (message) {
+                return message.toJSON();
+            });
+            res.send(transform);
+        });
+}
+module.exports.read = getAll;
+
+// get one message by id (req.params.id)
+function getOne(req, res, next) {
+    Message.findOne({_id: req.params.id}, function onMessageFound(err, message) {
+        if(!message) {
+            res.status(404).send("Message not found");
+        }
+
+        // we return the json version with cleaned up model to the user
+        res.send(message.toJSON());
+    });
+}
+module.exports.readOne = getOne;
+
+// create a new message (req.body)
+function add(req, res, next) {
+    var newMessage = new Message(req.body);
+
+    newMessage.save(function onMessageSaved(err, message) {
+        // we return the json version with cleaned up model to the user
+        res.send(message.toJSON());
+    });
+}
+module.exports.create = add;
+
+// update a message (req.body)
+function update(req, res, next) {
+    Message.findOneAndUpdate({_id: req.params.id}, function onMessageFound(err, message) {
+        if(!message) {
+            return res.status(404).send("Message not found");
+        }
+    });
+}
+module.exports.update = update;
+
+// remove one message by id (req.params.id)
+function remove(req, res, next) {
+    Message.findOneAndRemove({_id: req.params.id}, function onMessageFound(err, message) {
+        res.status(204).send();
+    });
+}
+module.exports.remove = remove;
+
